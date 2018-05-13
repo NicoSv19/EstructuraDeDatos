ae9b209725a25fdf112cc99b2a5527b2df536f42
DESKTOP-8V72MA2\Matias <yerlas12@gmail.com>
2018-05-12 23:22:13 -0300

    Taller1 modificado

--- Taller1/Sistema.h
+++ Taller1/Sistema.h
@@ -13,20 +13,11 @@ class Sistema {
 private:
 	ListGame listGames;
 	ListClient listClients;
-	Cola c;
 public:
 	Sistema();
 	virtual ~Sistema();
 	bool readGames();
 	bool readClient();
-	int regularBisiesto( Fecha fecha1 ); 
-	int diferenciaFechas(Fecha fecha1, Fecha fecha2);
-	int regularBisiesto( Fecha fechaRb );
-	void Carrito(Cliente* cliente);
-	void ProcesoPago(bool tienda, Cliente* cliente);
-	bool Tienda();
-	void NumeroVentaBodega();
-	void VentaBodega();
 	string printListGames();
 	string printListClient();
 
